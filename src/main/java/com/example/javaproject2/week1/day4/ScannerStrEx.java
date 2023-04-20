f44342658bf17dd9ef1658762833fad04597443b
happy <kjong0819@naver.com>
2023-04-20 10:24:36 +0900

    feat: Add class for string-to-integer conversion and addition

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrEx.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1.day4;
+import java.util.*;
+public class ScannerStrEx {
+
+    void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+
+    void inputStringsToNumbersSum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.parseInt(sc.next()) + Integer.parseInt(sc.next()));
+    }
+
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+        scannerStrEx.inputStringsToNumbersSum();
+    }
+}
